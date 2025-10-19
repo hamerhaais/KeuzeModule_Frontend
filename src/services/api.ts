d74e8df4019f339255d5bdcd7768196822240e79
hamerhaais <spn.braks@student.avans.nl>
2025-10-19 23:08:28 +0200

    frontend url no .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL: string = (import.meta as any).env.VITE_API_BASE_URL || 'http://localhost:3000';
+export const API_BASE_URL: string = (import.meta as any).env.VITE_API_BASE_URL || 'https://keuzemodule-backend-1.onrender.com/';
 
 export const endpoints = {
   modules: `${API_BASE_URL}/keuzemodules`,
